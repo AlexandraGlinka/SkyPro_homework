ad5608a29fd1187f7cd1dfb7f34b9006894676f1
glinka <alexandraglinka@yandex.ru>
2023-03-25 12:27:24 +0300

    hw13 toString

---
+++ src/hm13_methods/Author.java
@@ -0,0 +1,25 @@
+package hm13_methods;
+
+public class Author {
+    private String authorFirstName;
+    private String authorSurname;
+
+    public Author(String authorFirstName, String authorSurname) {
+        this.authorFirstName = authorFirstName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorFirstName() {
+        return this.authorFirstName;
+    }
+
+    public String getAuthorSurname() {
+        return this.authorSurname;
+    }
+
+    @Override
+    public String toString() {
+        return authorFirstName + " " + authorSurname;
+    }
+
+}
